597e0af7fa9bfd1f40a040853a47fc449f0daf3b
Peter Green <peter.green@aztek-native.com>
2015-02-17 15:37:22 +0000

    Include apache::default recipe, remove RHEL-specific config and drop in Debian-style config, enabling via apache_module definition.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe "apache2"
+
 if node['platform_family'] == "rhel"
   include_recipe "yum-epel"
 end
@@ -25,4 +27,23 @@ package pkg do
   action :install
 end
 
+# Remove the RPM packaged config if necessary
+file "#{node['apache']['dir']}/conf.d/mod_evasive.conf" do
+  action :delete
+  only_if { node['platform_family'] == "rhel" }
+end
+
+# Drop off debian-style config
+%w{ conf
+    load }.each do |ext|
+
+  template "#{node['apache']['dir']}/mods-available/evasive20.#{ext}" do
+    source "evasive.#{ext}.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+  end
+end
+
+apache_module "evasive20"
 
