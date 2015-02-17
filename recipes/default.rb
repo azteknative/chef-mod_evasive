34e9ca6625d8fc6e6ac9feb158aa2c48a64caf4b
Peter Green <peter.green@aztek-native.com>
2015-02-17 12:47:15 +0000

    Install the relevant package for the distribution.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+pkg = value_for_platform(
+  [ "centos", "fedora", "redhat" ] => {
+    "default" => "mod_evasive"
+  },
+  [ "debian", "ubuntu" ] => {
+    "default" => "libapache2-mod-evasive"
+  }
+)
+
+package pkg do
+  action :install
+end
+
+
